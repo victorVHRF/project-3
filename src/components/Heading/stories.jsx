5fc65120de95bf9db0d7983fb5e62c6a83830d54
Victor Hugo Rosa Fosneca <victor.vhrf@gmail.com>
2023-01-30 16:55:20 -0300

    implementado o primeiro component completo e configurado os snippets

---
+++ src/components/Heading/stories.jsx
@@ -0,0 +1,30 @@
+import { Heading } from '.'
+
+export default {
+  title: 'Heading',
+  component: Heading,
+  args: {
+    children: 'O texto esta escuro',
+  },
+  argTypes:{
+    children: {type: 'string'},
+  },
+  parameters:{
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+}
+
+export const Light = (args) => <Heading {...args} />
+export const Dark = (args) => <Heading {...args}/>
+
+Light.parameters = {
+  backgrounds:{
+    default: 'light'
+  }
+}
+Dark.args = {
+  children: 'O texto esta claro',
+  colorDark: false
+}
